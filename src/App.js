8aca4a542b89caabdd95a0a586da101766763fd2
Kubi Dogan <kubi_g@hotmail.co.uk>
2022-03-22 15:36:59 +0000

    added onClick function to flats and map

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      flats: []
+      flats: [],
+      selectedFlat: null
     };
   }
 
@@ -24,10 +25,23 @@ class App extends Component {
     })
   }
 
+  selectFlat = (flat) => {
+    this.setState({
+      selectedFlat: flat
+    })
+  }
+
   render() {
-    const center = {
+    let center = {
       lat: 48.8566,
       lng: 2.3522
+    }
+
+    if (this.state.selectedFlat) {
+      center = {
+        lat: this.state.selectedFlat.lat,
+        lng: this.state.selectedFlat.lng,
+      }
     }
     return (
       <div className="app">
@@ -36,7 +50,10 @@ class App extends Component {
           </div>
           <div className="flats">
             {this.state.flats.map((flat) => {
-              return <Flat flat={flat} />
+              return <Flat
+              key={flat.name}
+              flat={flat}
+              selectFlat={this.selectFlat}/>
             })}
           </div>
         </div>
@@ -46,7 +63,7 @@ class App extends Component {
         center={center}
         zoom={9}>
           {this.state.flats.map((flat) => {
-              return <Marker lat={flat.lat} lng={flat.lng} text={flat.price}/>
+              return <Marker key-={flat.name} lat={flat.lat} lng={flat.lng} text={flat.price}/>
             })}
         </GoogleMapReact>
         </div>
--- src/components/marker.js
+++ src/components/marker.js
@@ -3,8 +3,10 @@ import "./marker.css";
 
 class Marker extends React.Component {
   render () {
+    let classes = "marker";
+    if (this.props)
     return (
-      <div className="marker">
+      <div className="marker"> €
         {this.props.text}
       </div> );
   }
